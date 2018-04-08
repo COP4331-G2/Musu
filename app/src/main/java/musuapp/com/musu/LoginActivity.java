e839e14b8e5681addcff988d115e1b13c94fe987
grindlock <sergioenriqueperez@gmail.com>
2018-04-08 06:36:55 -0400

    Added a way to keep the app login. select keep me log in under login and it will.

--- app/src/main/java/musuapp/com/musu/LoginActivity.java
+++ app/src/main/java/musuapp/com/musu/LoginActivity.java
@@ -1,6 +1,8 @@
 package musuapp.com.musu;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,6 +10,7 @@ import android.util.Log;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     @InjectView(R.id.input_password) EditText _passwordText;
     @InjectView(R.id.btn_login) Button _loginButton;
     @InjectView(R.id.link_signup) TextView _signupLink;
+    @InjectView(R.id.keep_login) CheckBox _keepMeLogin;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,10 +42,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
 
+
         _loginButton  = findViewById(R.id.btn_login);
         _passwordText = findViewById(R.id.input_password);
         _emailText    = findViewById(R.id.input_email);
         _signupLink   = findViewById(R.id.link_signup);
+        _keepMeLogin  = findViewById(R.id.keep_login);
 
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
@@ -159,6 +165,8 @@ public class LoginActivity extends AppCompatActivity {
     public void onLoginSuccess() throws JSONException {
         _loginButton.setEnabled(true);
 
+        keepLogin();
+
         Intent intent = new Intent();
         JSONObject jsonResult = (JSONObject) LoginActivity.connJSON.get("results");
         intent.putExtra("userID", (int) jsonResult.get("userID"));
@@ -195,5 +203,18 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    public void keepLogin(){
+        boolean checked = _keepMeLogin.isChecked();
+        if(checked) {
+            //
+
+            SharedPreferences access = getSharedPreferences("Login", MODE_PRIVATE);
+            SharedPreferences.Editor editor = access.edit();
+            editor.putString("email", _emailText.getText().toString());
+            editor.putString("password", _passwordText.getText().toString());
+            editor.commit();
+        }
     }
 }
---
+++ app/src/main/java/musuapp/com/musu/SplashScreen.java
@@ -0,0 +1,148 @@
+package musuapp.com.musu;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final String TAG = "SplashScreen";
+    private static final int REQUEST_SIGNUP = 0;
+    private static JSONObject connJSON;
+    SharedPreferences access;
+
+    String email, password;
+
+    int currentUserID;
+
+    @InjectView(R.id.loading_text) TextView loadingText;
+    @InjectView(R.id.progressBar2) ProgressBar loadingProgressbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ButterKnife.inject(this);
+
+        access = getSharedPreferences("Login", MODE_PRIVATE);
+        email = access.getString("email", "");
+
+
+                if(email.length() != 0){
+                    password = access.getString("password", "");
+                    dologin();
+                }
+                else{
+                    onLoginFailed();
+                }
+
+    }
+
+
+    public  void login()
+    {
+        try {
+            onLoginSuccess();
+
+        }
+        catch(Exception e)
+        {
+            Log.e("JSON", "JSON Error: "+e.toString());
+        }
+    }
+    public void dologin() {
+        Log.d(TAG, "Login");
+
+        final databaseConnection conn = new databaseConnection();
+
+        JSONObject jsonTest = new JSONObject();
+
+        String serverName = getString(R.string.api_url);
+
+        try {
+            jsonTest.put("function", "loginAttempt");
+            jsonTest.put("username", email);
+            jsonTest.put("password", password);
+
+            Log.e("TEST (JSON payload): ", jsonTest.toString());
+
+            conn.execute(serverName, jsonTest.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        try {
+                            String connResult = conn.get();
+
+                            SplashScreen.connJSON = new JSONObject(connResult);
+
+                            Log.e("TEST (JSON result): ", connJSON.get("success").toString());
+
+                            if (connJSON.get("success").toString() == "true") {
+                                onLoginSuccess();
+                            }
+                            else{
+                                onLoginFailed();
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, 2000
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() throws JSONException {
+        JSONObject jsonResult = (JSONObject) SplashScreen.connJSON.get("results");
+        goToMain ((int) jsonResult.get("userID"));
+    }
+
+    public void onLoginFailed() {
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        try {
+                            goToMain(-1);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, 500
+        );
+    }
+
+    public void goToMain(int userID){
+        Log.i("ID",Integer.toString(userID));
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("userID", userID);
+        startActivity(intent);
+        finish();
+    }
+}
+
